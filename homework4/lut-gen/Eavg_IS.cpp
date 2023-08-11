7481f3ce2bc71e3c14216848203afc1cefda5ba7
Asuka <bingkan@outlook.com>
2023-08-11 14:33:23 +0800

    Kulla Conty pre compute

--- homework4/lut-gen/Eavg_IS.cpp
+++ homework4/lut-gen/Eavg_IS.cpp
@@ -1,6 +1,7 @@
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
 #include <algorithm>
+#define _USE_MATH_DEFINES
 #include <cmath>
 #include <sstream>
 #include <fstream>
@@ -28,14 +29,46 @@ Vec2f Hammersley(uint32_t i, uint32_t N) {
 Vec3f ImportanceSampleGGX(Vec2f Xi, Vec3f N, float roughness) {
 
     float a = roughness * roughness;
+    float x = Xi.x;
+    float y = Xi.y;
 
-    // TODO: Copy the code from your previous work - Bonus 1
+    //TODO: in spherical space - Bonus 1
+    // 
+    // uniform circle
+    float phi = 2.0 * M_PI * x;
+    // the less the roughness, the more cosTheta close to 1, and theta to 0
+    // so as roughness decrease, the point is more close to the sphere top
+    float cosTheta = std::sqrt((1.0 - y) / (1.0 + (a * a - 1.0) * y));
+    float sinTheta = std::sqrt(1.0 - cosTheta * cosTheta);
 
-    return Vec3f(1.0f);
+    //TODO: from spherical space to cartesian space - Bonus 1
+    // 
+    // half vector
+    Vec3f H = {};
+    const float r = 1.0;
+    H.x = r * sinTheta * std::cos(phi);
+    H.y = r * sinTheta * std::sin(phi);
+    H.z = r * cosTheta;
+
+    //TODO: tangent coordinates - Bonus 1
+    // 
+    // we need to generate a vector perpendicular to N as the tangent
+    // so first choose a *random* vector which is not N itself
+    Vec3f M = Vec3f(0.0, 1.0, 0.0);
+    N = normalize(N);
+    Vec3f T = normalize(cross(M, N));
+    Vec3f B = normalize(cross(T, N));
+
+    //TODO: transform H to tangent space - Bonus 1
+    // 
+    // This should be transform H to world space
+    Vec3f worldH = T * H.x + B * H.y + N * H.z;
+    return normalize(worldH);
 }
 
 
 Vec3f IntegrateEmu(Vec3f V, float roughness, float NdotV, Vec3f Ei) {
+
     Vec3f Eavg = Vec3f(0.0f);
     const int sample_count = 1024;
     Vec3f N = Vec3f(0.0, 0.0, 1.0);
@@ -52,10 +85,11 @@ Vec3f IntegrateEmu(Vec3f V, float roughness, float NdotV, Vec3f Ei) {
         float NoV = std::max(dot(N, V), 0.0f);
 
         // TODO: To calculate Eavg here - Bonus 1
-        
+        Eavg += Ei * NoL * 2.0;
     }
 
-    return Vec3f(1.0);
+    Eavg = Eavg / static_cast<float>(sample_count);
+    return Eavg;
 }
 
 void setRGB(int x, int y, float alpha, unsigned char *data) {
@@ -90,7 +124,10 @@ int main() {
         // | 
         // | rough（i）
         // Flip it, if you want the data written to the texture
-        uint8_t data[resolution * resolution * 3];
+        //uint8_t data[resolution * resolution * 3];
+        std::vector<uint8_t> data;
+        data.resize(resolution * resolution * 3);
+
         float step = 1.0 / resolution;
         Vec3f Eavg = Vec3f(0.0);
 		for (int i = 0; i < resolution; i++) 
@@ -103,18 +140,18 @@ int main() {
 
                 Vec3f Ei = getEmu((resolution - 1 - i), j, 0, Edata, NdotV, roughness);
                 Eavg += IntegrateEmu(V, roughness, NdotV, Ei) * step;
-                setRGB(i, j, 0.0, data);
+                setRGB(i, j, 0.0, data.data());
 			}
 
             for(int k = 0; k < resolution; k++)
             {
-                setRGB(i, k, Eavg, data);
+                setRGB(i, k, Eavg, data.data());
             }
 
             Eavg = Vec3f(0.0);
 		}
 		stbi_flip_vertically_on_write(true);
-		stbi_write_png("GGX_Eavg_LUT.png", resolution, resolution, channel, data, 0);
+		stbi_write_png("GGX_Eavg_LUT.png", resolution, resolution, channel, data.data(), 0);
 	}
 	stbi_image_free(Edata);
     return 0;
