387d6de79ccecf05bb323fb9217523c16fff8eab
Inori <bingkan@outlook.com>
2023-08-28 20:54:26 +0800

    joint bilateral filter

--- homework5/src/denoiser.cpp
+++ homework5/src/denoiser.cpp
@@ -1,4 +1,5 @@
 #include "denoiser.h"
+#include <cmath>
 
 Denoiser::Denoiser() : m_useTemportal(false) {}
 
@@ -42,11 +43,63 @@ Buffer2D<Float3> Denoiser::Filter(const FrameInfo &frameInfo) {
     int width = frameInfo.m_beauty.m_width;
     Buffer2D<Float3> filteredImage = CreateBuffer2D<Float3>(width, height);
     int kernelRadius = 16;
+
 #pragma omp parallel for
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
             // TODO: Joint bilateral filter
-            filteredImage(x, y) = frameInfo.m_beauty(x, y);
+
+            Float3 sum_of_weighted_sample = 0.0;
+            float sum_of_weight = 0.0;
+
+            auto pi = frameInfo.m_position(x, y);
+            auto ni = frameInfo.m_normal(x, y);
+            auto ci = frameInfo.m_beauty(x, y);
+            
+            for (int dy = -(kernelRadius - 1); dy <= (kernelRadius - 1); ++dy)
+            {
+                for (int dx = -(kernelRadius - 1); dx <= (kernelRadius - 1); ++dx)
+                {
+                    int x_j = x + dx;
+                    int y_j = y + dy;
+                    if (x_j < 0 || y_j < 0 || x_j >= width || y_j >= height)
+                    {
+                        continue;
+                    }
+
+                    auto pj = frameInfo.m_position(x_j, y_j);
+                    auto cj = frameInfo.m_beauty(x_j, y_j);
+                    auto nj = frameInfo.m_normal(x_j, y_j);
+
+                    float weight = 0.0;
+                    float distance = Distance(pi, pj);
+                    if (distance < std::numeric_limits<float>::epsilon())
+                    {
+                        // if two points are so close to each other such that we
+                        // think they are the same point, the weight should be 1.0
+                        weight = 1.0;
+                    } 
+                    else 
+                    {
+                        auto coord = -SqrDistance(pi, pj) / (2.0 * std::powf(m_sigmaCoord, 2.0));
+                        auto color = -SqrDistance(ci, cj) / (2.0 * std::powf(m_sigmaColor, 2.0));
+                        auto normal = -std::powf(SafeAcos(Dot(ni, nj)), 2.0) / (2.0 * std::powf(m_sigmaNormal, 2.0));
+
+                        auto ei = Normalize(pj - pi);
+                        auto plane = -std::powf(Dot(ni, ei), 2.0) / (2.0 * std::powf(m_sigmaPlane, 2.0));
+
+                        weight = std::expf(coord + color + normal + plane);
+                    }
+
+                    sum_of_weighted_sample += (cj * weight);
+                    sum_of_weight += weight;
+                }
+            }
+
+
+            //filteredImage(x, y) = frameInfo.m_beauty(x, y);
+            //filteredImage(x, y) = frameInfo.m_position(x, y);
+            filteredImage(x, y) = sum_of_weighted_sample / sum_of_weight;
         }
     }
     return filteredImage;
